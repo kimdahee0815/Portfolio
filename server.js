c62bd0496fba83e1d86d02d90090f2ae2efcc334
kimdahee0815 <billionairedahee@gmail.com>
2025-05-19 13:03:15 +0900

    feat: sendgrid to enveloop

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ app.post('/send-email', async (req, res) => {
 
   try {
     await enveloop.sendMessage({
-      template: `
+      html: `
     <div style="font-family: Arial, sans-serif; line-height: 1.6; padding: 20px; max-width: 600px; margin: auto; border: 1px solid #ddd; border-radius: 10px;">
       <h2 style="color: #333;">📬 New Message from <strong>${name}</strong></h2>
       <p><strong>Sender Email:</strong> ${email}</p>
